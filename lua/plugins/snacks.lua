5dfd5347ad24c5ff5b8004ef44f65b02cbb82a5a
8180e <emirceylan31@hotmail.com>
2026-06-29 16:58:11 +0200

    feat: add top pickers and explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,6 +9,49 @@ return {
 		indent = {},
 	},
 	keys = {
+		-- Top Pickers & Explorer
+		{
+			"<leader><space>",
+			function()
+				Snacks.picker.smart()
+			end,
+			desc = "Smart Find Files",
+		},
+		{
+			"<leader>,",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>/",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>:",
+			function()
+				Snacks.picker.command_history()
+			end,
+			desc = "Command History",
+		},
+		{
+			"<leader>n",
+			function()
+				Snacks.picker.notifications()
+			end,
+			desc = "Notification History",
+		},
+		{
+			"<leader>e",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "File Explorer",
+		},
 		-- find
 		{
 			"<leader>fb",
